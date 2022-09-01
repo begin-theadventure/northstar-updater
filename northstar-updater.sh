4a3e6d2d2b6e7edd21e456ec9325c1528ef7d418
begin-theadventure <99835765+begin-theadventure@users.noreply.github.com>
2022-09-01 13:16:37 +0200

    northstar-updater.sh: rm or mv ns_startup_args_..
    It moves ns_startup_args.txt ns_startup_args_dedi.txt to Titanfall2 or removes them (in northstar-updater folder) to not override.
    This is also why now it unzips to northstar-updater.

--- northstar-updater.sh
+++ northstar-updater.sh
@@ -4,12 +4,23 @@ echo "# Northstar updater. Place in the game directory and run."
 
 owner_repo='R2Northstar/Northstar'; latest_version_url="$(curl -s https://api.github.com/repos/$owner_repo/releases/latest| grep "browser_download_url.*zip" | cut -d : -f 2,3 | tr -d \")"; echo $latest_version_url; basename $latest_version_url ; curl -X GET $latest_version_url  -Lo northstar.zip
 
-echo "# Unzip northstar.zip and replace all files.."
-    unzip -o northstar.zip
+echo "# Unzip northstar.zip to northstar-updater.."
+    unzip -o northstar.zip -d northstar-updater
+
+echo "# Remove ns_startup_args.txt and ns_startup_args_dedi.txt in northstar-updater or move to Titanfall2 if it doesn't exist.."
+    cd northstar-updater
+    mv -vn ns_startup_args.txt ns_startup_args_dedi.txt ..
+    rm ns_startup_args.txt ns_startup_args_dedi.txt
+
+echo "# Copy changed files to Titanfall2."
+    cp -r * ..
 
 echo "# Update done! 🐸"
 
 echo "# Cleanup.."
+    cd ..
+echo "# Remove northstar-updater"
+    rm -dr northstar-updater
 echo "# Remove northstar.zip"
     rm northstar.zip
 
